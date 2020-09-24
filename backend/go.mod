272709dec1517714bab6a4bfcb9037394454ef08
buildingsite <worker1.buildingsite@gmail.com>
2020-09-24 19:02:15 +0200

    #1.2.1.0 Content backend

---
+++ backend/go.mod
@@ -0,0 +1,9 @@
+module backend
+
+go 1.15
+
+require (
+	github.com/urfave/cli v1.22.4
+	github.com/urfave/cli/v2 v2.2.0
+	go.mongodb.org/mongo-driver v1.4.1
+)
--- backend/main.go
+++ backend/main.go
@@ -3,10 +3,55 @@ package main
 import (
 	"context"
 	"log"
+	"os"
+	"time"
 
+	"github.com/urfave/cli"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type Task struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	CreatedAt time.Time          `bson:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at"`
+	Text      string             `bson:"text"`
+	Completed bool               `bson:"completed"`
+}
+
+func main() {
+	app := &cli.App{
+		Name:     "ungar",
+		Usage:    "backend for ungar",
+		Commands: []*cli.Command{
+			{
+                Name:    "add",
+                Aliases: []string{"a"},
+                Usage:   "add a task to the list",
+                Action: func(c *cli.Context) error {
+                    str := c.Args().First()
+                    if str == "" {
+                        return errors.New("Cannot add an empty task")
+                    }
+
+                    task := &Task{
+                        ID:        primitive.NewObjectID(),
+                        CreatedAt: time.Now(),
+                        UpdatedAt: time.Now(),
+                        Text:      str,
+                        Completed: false,
+                    }
+
+                    return createTask(task)
+                },
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 var collection *mongo.Collection
 var ctx = context.TODO()
@@ -17,4 +62,15 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection = client.Database("ungar").Collection("posts")
+}
+
+func createTask(task *Task) error {
+	_, err := collection.InsertOne(ctx, task)
+	return err
 }
