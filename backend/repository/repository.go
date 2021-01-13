cccebdc84c6117f0cefd2b055bf2878f6b0ec1a2
buildingsite <worker1.buildingsite@gmail.com>
2021-01-13 12:14:37 +0100

    successfully connected to mongo cloud atlas

--- backend/repository/repository.go
+++ backend/repository/repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -15,11 +16,23 @@ var ctx = context.TODO()
 
 func ConnectToMongoDB() {
 	var err error
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	ungarClient, err = mongo.Connect(ctx, clientOptions)
+	var ungarClient *mongo.Client
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	ungarClient, err = mongo.Connect(ctx, options.Client().ApplyURI(
+		"mongodb+srv://ungar:ungardatabase_123@cluster0.ykrvq.mongodb.net/<Ungar>?retryWrites=true&w=majority",
+	))
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	// ungarClient, err = mongo.Connect(ctx, clientOptions)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
 	err = ungarClient.Ping(context.TODO(), nil)
 	if err != nil {
 		log.Fatal(err)
