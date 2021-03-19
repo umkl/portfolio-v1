504c85373718f7ff4bf6491b1bc433b341b52dbf
ungarmichael <ungarmichael.mail@gmail.com>
2021-03-19 14:27:31 +0100

    ip-change

--- frontend/src/App/pages/Content/content.jsx
+++ frontend/src/App/pages/Content/content.jsx
@@ -9,7 +9,7 @@ import SearchIcon from "./../../assets/Searchicon.png";
 import "./content.scss";
 
 const Content = () => {
-  const API_URL = "http://10.0.0.11:8080/contributions";
+  const API_URL = "http://10.0.0.1:8080/contributions";
   // const [searchInput, setSearchInput] = useContext(SearchContext);
   const [contributions, setContributions] = useState([]);
   const [isLoaded, setLoaded] = useState(false);
